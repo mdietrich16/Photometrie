0e1f041b92e0f5d69b390c58513ff558ab848b3c
Maximilian Dietrich <m.dietrich16@web.de>
2021-12-11 14:27:48 +0100

    Finished!

--- fits.py
+++ fits.py
@@ -11,6 +11,7 @@ import datetime
 jdt_exo = 2.459492e6
 jdt_var = 2.459543e6
 
+
 def jul_to_greg(jd):
     assert np.all(jd >= 0)
     frac, jd = np.modf(jd)
@@ -27,14 +28,19 @@ def jul_to_greg(jd):
     frac, microsecond = np.modf(1000000 * frac)
     return datetime.datetime(year=year, month=month, day=day, hour=12+int(hour), minute=int(minute), second=int(second), microsecond=int(microsecond))
 
+
 def greg_to_jul(dt):
     # jdn = (1461 * (dt.year + 4800 + (dt.month - 14) // 12)) // 4 + (367 * (dt.month - 2 - 12 * ((dt.month - 14) // 12))) // 12 - (3 * ((dt.year + 4900 + (dt.month - 14) // 12) // 100)) // 4 + dt.day - 32075
     # return float(jdn) + float(dt.hour - 12)/24 + float(dt.minute)/1440 + float(dt.second)/86400 + float(dt.microsecond)/1000000
-    jd = 367 * dt.year - int((7 * (dt.year + int((dt.month + 9) / 12.0))) / 4.0) + int((275 * dt.month) / 9.0) + dt.day + 1721013.5 +(dt.hour + dt.minute / 60.0 + dt.second / np.power(60,2) + dt.microsecond / (np.power(60,2) * 1000000)) / 24.0 - 0.5 * np.copysign(1, 100 * dt.year + dt.month - 190002.5) + 0.5
+    jd = 367 * dt.year - int((7 * (dt.year + int((dt.month + 9) / 12.0))) / 4.0) + int((275 * dt.month) / 9.0) + dt.day + 1721013.5 + (dt.hour + dt.minute /
+                                                                                                                                       60.0 + dt.second / np.power(60, 2) + dt.microsecond / (np.power(60, 2) * 1000000)) / 24.0 - 0.5 * np.copysign(1, 100 * dt.year + dt.month - 190002.5) + 0.5
     return jd
 
+
 def trapez(x, p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y):
-    condlist = [np.logical_or(x < p0x, x > p3x), np.logical_and(x < p1x, x > p0x), np.logical_and(x < p2x, x > p1x), np.logical_and(x < p3x, x > p2x)]
+    condlist = [np.logical_or(x < p0x, x > p3x), np.logical_and(
+        x < p1x, x > p0x), np.logical_and(x < p2x, x > p1x), np.logical_and(x < p3x, x > p2x)]
+
     def steady(x):
         return (p3y - p0y) / (p3x - p0x) * (x - p0x) + p0y
 
@@ -49,6 +55,7 @@ def trapez(x, p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y):
 
     return np.piecewise(x, condlist=condlist, funclist=[steady, trans_in, trans_total, trans_out])
 
+
 def transit(x, m, b, tm, tt, td, ttd):
     # x: Input
     # m: Steigung der Hauptgeraden
@@ -61,43 +68,52 @@ def transit(x, m, b, tm, tt, td, ttd):
     td2, ttd2 = td/2, ttd/2
     diff = td2 - ttd2
     slope = tt / diff
-    condlist = [np.logical_and(x > tm-td/2, x < tm-ttd/2), np.logical_and(x > tm-ttd/2, x < tm+ttd/2), np.logical_and(x > tm+ttd/2, x < tm+td/2)]
-    y += np.piecewise(x, condlist, funclist=[lambda x: -slope * (x - (tm - td2)), lambda x: -tt, lambda x: slope * (x - (tm + td2)), lambda x: 0])
+    condlist = [np.logical_and(x > tm-td/2, x < tm-ttd/2), np.logical_and(
+        x > tm-ttd/2, x < tm+ttd/2), np.logical_and(x > tm+ttd/2, x < tm+td/2)]
+    y += np.piecewise(x, condlist, funclist=[lambda x: -slope * (
+        x - (tm - td2)), lambda x: -tt, lambda x: slope * (x - (tm + td2)), lambda x: 0])
     return y
+
 
 def lerp(t, p1, p2):
     return (1 - t)*p1 + t*p2
 
+
 def nCk(n, k):
     return factorial(n)/(factorial(n-k)*factorial(k))
+
 
 def bezier(t, points):
     t = t.reshape(-1, 1)
     degree = points.shape[0]-1
     i = np.tile(np.arange(degree+1), (t.shape[0], 1))
-    return np.dot(nCk(degree, i)*np.power(1 - t, degree - i)*np.power(t,i),points)
+    return np.dot(nCk(degree, i)*np.power(1 - t, degree - i)*np.power(t, i), points)
 
-#def bezier(t, points):
+# def bezier(t, points):
 #    num = len(points)
 #    if num == 1:
 #        return points[0]
 #    else:
 #        return lerp(t, bezier(t, points[:-1]), bezier(t, points[1:]))
 
+
 def transit_bezier(x, x0, xn, *points):
     dx = 0.05
     points = np.array(points)
     x = np.array(x)
-    dx2= xn - x0
+    dx2 = xn - x0
     # return m * x + b + (x > x0)*(x < xn)*bezier((x-x0)/dx, points)[:,1]
     # return bezier((x-x0)/dx, points)
+
     def linear(x):
         return (points[-1] - points[0])/dx2 * (x - x0) + points[0]
 
-    points = np.array([points[0], linear(x0 + dx), *points[1:-1], linear(xn - dx), points[-1]])
+    points = np.array([points[0], linear(x0 + dx), *
+                      points[1:-1], linear(xn - dx), points[-1]])
     condlist = [x < x0, np.logical_and(x >= x0, x < xn), x >= xn]
     funclist = [linear, lambda z: bezier((z-x0)/dx2, points), linear]
     return np.piecewise(x, condlist, funclist)
+
 
 def fourier(x, *params):
     params = np.array(params)
@@ -110,20 +126,22 @@ def fourier(x, *params):
     offset = params[2]
     coeffs = params[3:]
     k = 2*np.pi / T
-    ts = k * np.atleast_2d(np.arange(1, n//2+1)).transpose().dot(np.atleast_2d(x))
+    ts = k * np.atleast_2d(np.arange(1, n//2+1)
+                           ).transpose().dot(np.atleast_2d(x))
     s = np.sin(ts)
     c = np.cos(ts)
-    return coeffs.dot(np.concatenate((s,c))) + m*x + offset
+    return coeffs.dot(np.concatenate((s, c))) + m*x + offset
 
 
 def load_data(file):
     rawdata = np.genfromtxt(file, delimiter=',', skip_header=1)
     t = rawdata[:, 0]
-    VC = np.power(10,-rawdata[:, 1]/2.5)
+    VC = np.power(10, -rawdata[:, 1]/2.5)
     # 10^(-x/2.5)' = e^(ln10 * -x/2.5)' = -ln(10)/2.5 * e^(ln10 * -x/2.5)
-    VCerr = np.log(10)/2.5 * np.power(10,-rawdata[:, 1]/2.5) * rawdata[:, 2]
+    VCerr = np.log(10)/2.5 * np.power(10, -rawdata[:, 1]/2.5) * rawdata[:, 2]
     data = np.stack((t, VC, VCerr), axis=-1)
     return data
+
 
 def plot_data(data, date, fit_funcs, fit_params, labels, star, filename=None):
     t, VC, VCerr = data.transpose()
@@ -133,7 +151,8 @@ def plot_data(data, date, fit_funcs, fit_params, labels, star, filename=None):
     #plt.errorbar(x=jul_to_greg(t), y=VC, yerr= VCerr, fmt='x')
     plt.figure(figsize=(14, 10), dpi=300)
 
-    plt.errorbar(x=t, y=VC, yerr= VCerr, fmt='.', elinewidth=.8, capsize=2, label='Daten')
+    plt.errorbar(x=t, y=VC, yerr=VCerr, fmt='.',
+                 elinewidth=.8, capsize=2, label='Daten')
 
     for i, fit_func in enumerate(fit_funcs):
         fit = fit_func(t, *(fit_params[i]))
@@ -152,10 +171,11 @@ def plot_data(data, date, fit_funcs, fit_params, labels, star, filename=None):
     else:
         plt.show()
 
+
 if __name__ == "__main__":
     exodata = load_data('./Tres-1b/Tres-1b_V-C.csv')
     jdt_exo = int(exodata[:, 0].mean())
-    exodata[:,0] -= jdt_exo
+    exodata[:, 0] -= jdt_exo
     t_exo = exodata[:, 0]
     VC_exo = exodata[:, 1]
     vardata = load_data('./UCAC4_558-007313/UCAC4_558-007313_Light_Curve.csv')
@@ -167,29 +187,36 @@ if __name__ == "__main__":
     p0tr = np.array([0.33, 1.0, 0.35, 1.0, 0.425, 1.0, 0.44, 1.0])
     pt0 = np.array([0., 1.0, 0.375, 0.2, 0.1, 0.05])
     pb0 = np.array([0.385, 0.425, 1.0, 0.9, 0.9, 0.9, 1.0])
-    
+
     # Hier bitte symmetrischen Trapez-Fit
     paramstr, paramstrcov = opt.curve_fit(trapez, t_exo, VC_exo, p0=p0tr)
     paramst, paramstcov = opt.curve_fit(transit, t_exo, VC_exo, p0=pt0)
     paramsb, paramsbcov = opt.curve_fit(transit_bezier, t_exo, VC_exo, p0=pb0)
-    plot_data(exodata, jdt_exo, [trapez, transit, transit_bezier], [paramstr, paramst, paramsb], ['Trapez-Fit', 'symmetrischer Trapez-Fit', 'Bézier-Fit'], 'UCAC4 558-007131', filename='UCAC4.png')
+    plot_data(exodata, jdt_exo, [trapez, transit, transit_bezier], [paramstr, paramst, paramsb], [
+              'Trapez-Fit', 'symmetrischer Trapez-Fit', 'Bézier-Fit'], 'Tres-1b', filename='Tres-1b.png')
 
     # Ordnung 7 sieht gut aus mit Gerade
-    params6, params6cov = opt.curve_fit(fourier, t_var, VC_var, p0=[0.15, -0.1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
-    params2, params2cov = opt.curve_fit(fourier, t_var, VC_var, p0=[0.15, -0.1, 0.5, 0.5, 0.5, 0.5, 0.5])
-    params1, params1cov = opt.curve_fit(fourier, t_var, VC_var, p0=[0.15, -0.1, 0.5, 0.5, 0.5])
-    plot_data(vardata, jdt_var, [fourier, fourier, fourier], [params6, params2, params1], ['Fourier-Fit 6.Ordnung', 'Fourier-Fit 2.Ordnung', 'Fourier-Fit 1.Ordnung'], 'Tres-1b', filename='Tres-1b.png')
-    
+    params6, params6cov = opt.curve_fit(fourier, t_var, VC_var, p0=[
+                                        0.15, -0.1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+    params2, params2cov = opt.curve_fit(fourier, t_var, VC_var, p0=[
+                                        0.15, -0.1, 0.5, 0.5, 0.5, 0.5, 0.5])
+    params1, params1cov = opt.curve_fit(fourier, t_var, VC_var, p0=[
+                                        0.15, -0.1, 0.5, 0.5, 0.5])
+    plot_data(vardata, jdt_var, [fourier, fourier, fourier], [params6, params2, params1], [
+              'Fourier-Fit 6.Ordnung', 'Fourier-Fit 2.Ordnung', 'Fourier-Fit 1.Ordnung'], 'UCAC4 558-007131', filename='UCAC4.png')
+
     exoparams = paramst
     exostd = np.sqrt(np.diag(paramstcov))
     varparams = params6
     varstd = np.sqrt(np.diag(params6cov))
 
     print("Exoplanet-Transit:")
-    print("Transittiefe:[W]", exoparams[3], "+-", exostd[3])
-    print("relative Transittiefe:", exoparams[3]/exoparams[1], "+-", np.sqrt(np.square(exostd[3]/exoparams[1]) + np.square(exoparams[3]*exostd[1]/exoparams[1]**2)))
+    print("Transittiefe:[relativer Strahlungsfluss]",
+          exoparams[3], "+-", exostd[3])
+    print("relative Transittiefe:", exoparams[3]/exoparams[1], "+-", np.sqrt(np.square(
+        exostd[3]/exoparams[1]) + np.square(exoparams[3]*exostd[1]/exoparams[1]**2)))
     print("Transit-Duration[d]:", exoparams[4], "+-", exostd[4])
     print("Totalitätsdauer[d]:", exoparams[5], "+-", exostd[5])
-    
+
     print("Bedeckungsveränderlicher:")
     print("Periodendauer:", varparams[0], "+-", varstd[0])
---
+++ plots.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_data(filename):
+    rawdata = np.genfromtxt(filename, delimiter=' ', skip_header=2)
+    t = rawdata[:, 0]
+    data = rawdata[:, 1::2]
+    errs = rawdata[:, 2::2]
+    mean = np.mean(data, axis=0)
+    mask = np.abs(data - mean) / mean > 2
+    if np.any(mask):
+        mask = np.any(mask, axis=1).nonzero()[0]
+        t = np.delete(t, mask, 0)
+        errs = np.delete(errs, mask, 0)
+        data = np.delete(data, mask, 0)
+    t -= t[0]
+    data = np.power(10, -data/2.5)
+    errs = np.log(10)/2.5 * np.power(10, -data/2.5) * errs
+    return t, data, errs
+
+
+def plot_data(t, data, errs, n, k, name):
+
+    # V vs k
+    kwargs = {"xlabel": "Zeit in Tagen"}
+    figVK, axVK = plt.subplots(
+        nrows=k, ncols=1, subplot_kw=kwargs, layout='constrained', figsize=(14, 10), dpi=200, sharex='col')
+    figVK.suptitle("Strahlungsflussverhältnisse V vs K")
+    for i in range(k):
+        axVK[i].errorbar(x=t, y=data[:, 1+i], yerr=errs[:, 1+i],
+                         fmt='.', markersize=4, elinewidth=.8, capsize=1)
+        axVK[i].set_ylabel("$\\frac{S_V}{S_{K" + str(i+1) + "}}$", fontsize=20)
+        axVK[i].grid(True, color='gray', linewidth=0.1)
+
+    figVK.savefig(name + "_VK.png", dpi=200, bbox_inches='tight')
+
+    # C vs k
+    figCK, axCK = plt.subplots(
+        n, k, subplot_kw=kwargs, layout='constrained', figsize=(14, 10), dpi=200, sharex='col')
+    figCK.suptitle("Strahlungsflussverhältnisse C vs K")
+    for i in range(n):
+        for j in range(k):
+            axCK[i, j].errorbar(
+                t, data[:, 1+k+i*k+j], errs[:, 1+k+i*k+j], fmt='.', markersize=4, elinewidth=.8, capsize=1)
+            axCK[i, j].set_ylabel(
+                "$\\frac{S_{C" + str(i+1) + "}}{S_{K" + str(j+1) + "}}$", fontsize=20)
+            axCK[i, j].grid(True, color='gray', linewidth=0.1)
+
+    figCK.savefig(name + "_CK.png", dpi=200, bbox_inches='tight')
+
+    # K vs K
+    figKK, axKK = plt.subplots(
+        n*(n-1)//2, 1, subplot_kw=kwargs, layout='constrained', figsize=(14, 10), dpi=200, sharex='col')
+    figKK.suptitle("Strahlungsflussverhältnisse K vs K")
+    for i in range(n*(n-1)//2):
+        axKK[i].errorbar(t, data[:, 1+k+k*n+i], errs[:, 1+k +
+                         k*n+i], fmt='.', markersize=4, elinewidth=.8, capsize=1)
+        x = int(0.5 + np.sqrt(0.25 + 2*i))
+        y = i - x*(x-1)//2
+        axKK[i].set_ylabel(
+            "$\\frac{S_{K" + str(x + 1) + "}}{S_{K" + str(y + 1) + "}}$", fontsize=20)
+        axKK[i].grid(True, color='gray', linewidth=0.1)
+
+    figKK.savefig(name + "_KK.png", dpi=200, bbox_inches='tight')
+    # plt.show()
+
+
+if __name__ == '__main__':
+    t, data, errs = load_data(
+        'UCAC4_558-007313/UCAC4_558-007313_Light_curve.txt')
+    plot_data(t, data, errs, 3, 3, 'UCAC')
